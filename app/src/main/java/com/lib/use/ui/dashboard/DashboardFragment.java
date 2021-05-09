7682e41b3c49800dd28edebb267c8d96ec82be5c
e3control-zhanglimin <limin.zhang@e3control.cn>
2021-05-09 15:38:17 +0800

    使用系统文件管理器导出文件到公共目录

--- app/src/main/java/com/lib/use/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/lib/use/ui/dashboard/DashboardFragment.java
@@ -1,7 +1,12 @@
 package com.lib.use.ui.dashboard;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.provider.DocumentsContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +22,17 @@ import com.lib.android.base.ui.FileSelectActivity;
 import com.lib.use.R;
 import com.lib.use.databinding.FragmentDashboardBinding;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static android.app.Activity.RESULT_OK;
+
 public class DashboardFragment extends Fragment {
+    private static final String TAG = "DashboardFragment";
+    // Request code for creating a PDF document.
+    private static final int CREATE_FILE = 1;
 
     private DashboardViewModel dashboardViewModel;
     private FragmentDashboardBinding binding;
@@ -39,6 +54,55 @@ public class DashboardFragment extends Fragment {
             startActivity(intent);
         });
 
+        binding.btnCreateFile.setOnClickListener(v -> {
+            createFile("share.json");
+        });
+
         return binding.getRoot();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CREATE_FILE){
+            if (resultCode == RESULT_OK){
+                Uri uri = data.getData();
+                Log.e(TAG, "create file: "+ uri.getPath() );
+//                File file = new File(uri.getPath());
+                alterDocument(uri);
+            }
+        }
+    }
+
+    private void createFile(String name){
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+//        intent.setDataAndType(Uri.fromFile(Environment.getExternalStorageDirectory()), "*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("application/json");
+        intent.putExtra(Intent.EXTRA_TITLE, name);
+
+        // Optionally, specify a URI for the directory that should be opened in
+        // the system file picker when your app creates the document.
+//            intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri);
+
+        startActivityForResult(intent, CREATE_FILE);
+    }
+
+    private void alterDocument(Uri uri) {
+        try {
+            ParcelFileDescriptor pfd = getActivity().getContentResolver().
+                    openFileDescriptor(uri, "w");
+            FileOutputStream fileOutputStream =
+                    new FileOutputStream(pfd.getFileDescriptor());
+            fileOutputStream.write(("Overwritten at " + System.currentTimeMillis() +
+                    "\n").getBytes());
+            // Let the document provider know you're done by closing the stream.
+            fileOutputStream.close();
+            pfd.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
